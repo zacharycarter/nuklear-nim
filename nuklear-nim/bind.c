14a66e56e445a11611ce1bdd1f75e2ea2aa7403c
Zachary Carter <carterza@gmail.com>
2017-02-20 10:17:14 -0500

    Adding nuklear as submodule.

--- nuklear-nim/bind.c
+++ nuklear-nim/bind.c
@@ -7,7 +7,7 @@
 #define NK_INCLUDE_FONT_BAKING
 #define NK_IMPLEMENTATION
 
-#include "nuklear.h"
+#include "nuklear/nuklear.h"
 
 extern int nk_filter_my(const struct nk_text_edit *box, nk_rune unicode) {
 	return 0;
